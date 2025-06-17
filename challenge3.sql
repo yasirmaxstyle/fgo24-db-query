cf578127af07790988779fff26fb83e220ac44fc
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-17 16:24:57 +0700

    feat: add solution to challenge 3 no. 2

--- challenge3.sql
+++ challenge3.sql
@@ -7,6 +7,11 @@ group by
     d.id;
 
 -- 2. Mendapatkan data actor yang memiliki roles lebih dari 5
+select a.id, a.first_name, a.last_name, count(*) as roles_count
+from actors a
+    join roles r on r.actor_id = a.id
+group by
+    a.id;
 
 -- 3. Mendapatkan directors paling produktif sepanjang masa
 
