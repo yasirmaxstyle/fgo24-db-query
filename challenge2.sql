0fdc6cb63d5484abf53b62faca8e9424016290fa
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-17 11:10:21 +0700

    feat: add more join query

--- challenge2.sql
+++ challenge2.sql
@@ -1,7 +1,8 @@
 -- 1. Melakukan join director, genre ke tabel movies, dapatkan hanya 5 movie pertama saja
 
-select distinct m.name, d.first_name, d.last_name, dg.genre from movies m
+select distinct m.name as title, d.first_name, d.last_name, mg.genre from movies m
 join movies_directors md on md.movie_id = m.id
 join directors d on d.id = md.director_id
 join directors_genres dg on dg.director_id = md.director_id
-limit 5;
+join movies_genres mg on mg.movie_id = m.id and dg.genre = mg.genre
+limit 10;
