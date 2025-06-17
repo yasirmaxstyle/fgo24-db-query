f09dcc9a353a85f44c07fedb2ed2a9a48e6e825b
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-17 10:05:56 +0700

    feat: add challenge 1 task number 3

--- challenge1.sql
+++ challenge1.sql
@@ -3,3 +3,8 @@ select * from movies where year > 2000;
 
 -- 2. Mencari actor dengan akhiran nama 's'
 select * from actors where last_name ilike '%s';
+
+-- 3. Mencari movie dengan rating di antara 5 dan 7 tahun rilis 2004 sampai 2006
+--    urutkan dari yang tertinggi ratingnya
+select * from movies where rankscore between 5 and 7
+and year between 2004 and 2006 order by rankscore desc;
