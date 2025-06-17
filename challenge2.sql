732a3fdba05fa19ba4fcf05d2cbdfcd6c7bab827
yasirmaxstyle <yasirmu77@gmail.com>
2025-06-17 11:22:13 +0700

    feat: sort name actor by first name in ascending order

--- challenge2.sql
+++ challenge2.sql
@@ -6,3 +6,9 @@ join directors d on d.id = md.director_id
 join directors_genres dg on dg.director_id = md.director_id
 join movies_genres mg on mg.movie_id = m.id and dg.genre = mg.genre
 limit 50;
+
+-- 2. Melakukan join movie dan roles berdasarkan table actors.
+select a.id, a.first_name, a.last_name, a.gender, m.name as movie from actors a
+join roles r on r.actor_id = a.id
+join movies m on m.id = r.movie_id
+order by a.first_name asc limit 10;
